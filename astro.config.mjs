3f58fbc96054b64fa1ee91bfa2224457499eef92
Lukas Frey <lukas.frey03@outlook.de>
2023-03-18 02:51:03 +0100

    add chatgpt connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [
     tailwind(),
     image({
---
+++ src/pages/openai.json.ts
@@ -0,0 +1,33 @@
+import type { APIRoute } from "astro";
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  organization: import.meta.env.OPENAI_ORGANIZATION,
+  apiKey: import.meta.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export const post: APIRoute = async ({ request }) => {
+  const formData = await request.formData();
+  // only use first 100 characters of question
+  const question = (formData.get("q") as string).slice(0, 100);
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Antworte die Frage als wärst du eine Kuh. Bitte antworte fröhlich, etwas lustig und sehr knapp und nutze das Wort "muh".
+        
+        ${question}`,
+      },
+    ],
+
+    max_tokens: 150,
+  });
+
+  return new Response(JSON.stringify(response.data.choices[0]), {
+    status: 200,
+  });
+};
