d5ee87626adc9367b993ec5e95aebf24d6f53ae3
Lukas Frey <lukas.frey03@outlook.de>
2023-08-08 02:06:43 +0200

    move to edge runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/edge";
 import react from "@astrojs/react";
 
@@ -9,9 +8,6 @@ export default defineConfig({
   output: "server",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     react(),
   ],
   adapter: vercel({
